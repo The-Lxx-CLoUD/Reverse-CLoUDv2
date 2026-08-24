691386b3d16d303461c012f778eb7ccccb7da0f1
The Lxx CLoUD <cloudgithuuhuh@outlook.com>
2026-08-24 07:19:52 +0330

    README.md

--- c2beta.py
+++ c2beta.py
@@ -745,6 +745,8 @@ def execute_local(cmd_text):
     elif low == "sg1":
         send_msg_markdown(
             "📚 **Multi-Session Commands**\n"
+
+            "You can submit PowerShell commands here.\n"
             
             "**Capture:**\n"
             "`screenshot` — Taking a screenshot 📸\n"
